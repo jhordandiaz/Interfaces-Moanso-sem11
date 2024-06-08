6917e26fa83422620664439570f6b6f03140a989
Jose Diego Rodriguez Vasquez <N00319804@upn.pe>
2024-06-07 22:11:25 -0500

    Primer Cambio de la interfaz de SalidaProducto.cs

--- interfaces de proyecto/SistemaAlmacen/SalidaProducto.Designer.cs
+++ interfaces de proyecto/SistemaAlmacen/SalidaProducto.Designer.cs
@@ -28,19 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Matura MT Script Capitals", 28.2F);
+            this.label1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(423, 51);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Productos Solicitados: ";
             // 
             // SalidaProducto
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(785, 566);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SalidaProducto";
             this.Text = "SalidaProducto";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
