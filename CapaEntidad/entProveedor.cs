8f8161ae3c87706244ed7150288fefb7e7af57d1
jhordandiaz <146855483+jhordandiaz@users.noreply.github.com>
2024-06-22 20:56:02 -0500

    Capa Datos, Entidad, Logica

---
+++ CapaEntidad/entProveedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entProveedor
+    {
+        public int ProveedorID { get; set; }
+        public int CiudadID { get; set; }
+        public string Nombre { get; set; }
+        public int RubroID { get; set; }
+        public int Ruc { get; set; }
+        public Boolean estProveedor { get; set; }
+
+    }
+}
